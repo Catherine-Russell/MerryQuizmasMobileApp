4ead068b41927094bf212ed8e1407269dcfb9f83
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-07 23:16:28 +0000

    deletes unused imports following file rearrangement

--- app/src/main/java/com/example/quizproject/NextButton.kt
+++ app/src/main/java/com/example/quizproject/NextButton.kt
@@ -1,14 +1,8 @@
 package com.example.quizproject
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun NextButton(correctAnswer: Boolean?) {
