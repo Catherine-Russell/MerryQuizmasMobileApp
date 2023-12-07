205ea391ae9dea535707b14b4dd9a87e3d68c43f
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-07 13:36:03 +0000

    creates files for each composable on the Appscreen and creates a simple version of it in Appscreen in MainActivity

---
+++ app/src/main/java/com/example/quizproject/Header.kt
@@ -0,0 +1,9 @@
+package com.example.quizproject
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Header() {
+    Text(text = "This is the Header composable")
+}
--- app/src/main/java/com/example/quizproject/MainActivity.kt
+++ app/src/main/java/com/example/quizproject/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.quizproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quizproject.ui.theme.QuizProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppScreen()
                 }
             }
         }
@@ -30,17 +34,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello there $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuizProjectTheme {
-        Greeting("Android")
+fun AppScreen () {
+    Column {
+        Header()
+        Spacer(modifier = Modifier.height(20.dp))
+        Question()
+        Spacer(modifier = Modifier.height(20.dp))
+        Answer()
+        NextButton()
     }
 }
