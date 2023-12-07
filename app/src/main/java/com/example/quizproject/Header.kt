4c8ed939a7a7eae05c10847d650d502700c25ecb
David O'Donnell <dave@soundmotives.net>
2023-12-07 14:24:52 +0000

    Creation of initial Header composable

--- app/src/main/java/com/example/quizproject/Header.kt
+++ app/src/main/java/com/example/quizproject/Header.kt
@@ -1,9 +1,43 @@
 package com.example.quizproject
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
 
+@Preview
 @Composable
 fun Header() {
-    Text(text = "This is the Header composable")
+    Column {
+        val imageModifier = Modifier.fillMaxWidth()
+//        val title = textValue
+        Image(
+            painter = painterResource(id = R.drawable.quiztime),
+            contentDescription = "quiz logo",
+            modifier = imageModifier
+        )
+        Spacer(modifier = Modifier.height(20.dp)
+        )
+        Text(
+            text = "Welcome to the fantastic 1 question quiz!",
+            fontSize = 30.sp,
+            lineHeight = 1.2.em,
+            textAlign = TextAlign.Center,
+            )
+    }
 }
+
+
+
+
+
