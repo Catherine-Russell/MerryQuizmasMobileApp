1a7db9544be78c03fab9115d467a8a25b7261169
Catherine Russell <catherinerussell123@hotmail.co.uk>
2023-12-07 16:54:27 +0000

    Updates page with question 1 and deletes print statements for debugging

--- app/src/main/java/com/example/quizproject/MainActivity.kt
+++ app/src/main/java/com/example/quizproject/MainActivity.kt
@@ -3,20 +3,14 @@ package com.example.quizproject
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,23 +36,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 @Preview(showBackground = true)
 fun AppScreen () {
-    var correctAnswer: Boolean? by remember {
-        mutableStateOf(null)
-    }
-
-    val modifier = Modifier
-        .fillMaxSize()
-        .padding(20.dp)
-        .offset(0.dp, (-40).dp)
-
-    Column (modifier = modifier, verticalArrangement = Arrangement.Center) {
+    Column {
         Header()
         Spacer(modifier = Modifier.height(20.dp))
-        Question(correctAnswer, onValueChanged = {
-            correctAnswer = it)
+        Question()
         Spacer(modifier = Modifier.height(20.dp))
-        Answer(correctAnswer)
-        })
+        Answer()
         NextButton()
     }
 }
